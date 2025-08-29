69867356379a279e7f920952e1a5aa0f21b8e702
Erick Gonzalez <gempro30yt@gmail.com>
2025-08-29 16:04:50 -0300

    TP3 E1
    Realice E1 del TP3

--- VISTA/2/E4/index.php
+++ VISTA/2/E4/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../../../Boostrap-CSS/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../../CSS/2/E4/style.css">
     <title>Empresa Cinem@s</title>
 </head>
@@ -106,7 +106,7 @@
             
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="../../../Boostrap-JS/bootstrap.bundle.min.js"></script>
     <script src="validaciones.js"></script>
 </body>
 </html>
---
+++ VISTA/3/E1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../Boostrap-CSS/bootstrap.min.css">
+    <link rel="stylesheet" href="../../CSS/3/E1/layout.css">
+    <title>Subir Archivo</title>
+</head>
+<body>
+<div class="d-flex justify-content-center align-items-center vh-100">
+        <div class=" container">
+            <h3>Formulario subida de archivos...</h3>
+            <form name="form-subir" id="form-subir" method="POST" action="../../../CONTROL/3/E1/validarTipoDoc.php" enctype="multipart/form-data">
+                <div class="mb-3 input-file">
+                    <input type="file" name="form-subir">
+                </div>
+
+                <div class="button-form">
+                    <button class="btn btn-dark" type="submit">Subir Archivo</button>
+                </div>
+            </form>
+        </div>
+</div>
+
+
+    <script href="../../../Boostrap-JS/bootstrap.bundle.min.js"></script>
+</body>
+</html>
