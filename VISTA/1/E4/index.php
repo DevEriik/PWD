17f80739b5f54f36e5d5e22f39f94f95c53fecb3
Erick Gonzalez <gempro30yt@gmail.com>
2025-09-04 20:07:51 -0300

    Update TP1 E4

---
+++ VISTA/1/E4/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos Ejercicio 4</title>
+</head>
+<body>
+    <form id="form1" name="form1" method="get" action="../../../CONTROL/1/E4/form2.php">
+            Nombre: <input name="nombre" type="text" id="nombre" /> <br />
+            Apellido: <input name="apellido" type="text" id="apellido" /> <br />
+            Edad: <input name="edad" type="text" id="edad" /> <br />
+            Direccion: <input name="direccion" type="text" id="direccion" /> <br />
+             <input type="submit" name="Submit" value="Aceptar" />
+    </form>
+</body>
+</html>
