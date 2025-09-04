87ee3f652b6f9bcb1a7c48847fdee758985ab89c
Erick Gonzalez <gempro30yt@gmail.com>
2025-09-04 20:04:47 -0300

    Update T1 E3

---
+++ VISTA/1/E3/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Datos E3</title>
+</head>
+<body>
+    <!-- Con POST
+<html>
+    <head>
+    <title> Ejercicio 3 </title>
+    </head>
+    <body>
+         <form id="form1" name="form1" method="post" action="../../../CONTROL/1/E3/form.php">
+            Nombre: <input name="nombre" type="text" id="nombre" /> <br />
+            Apellido: <input name="apellido" type="text" id="apellido" /> <br />
+            Edad: <input name="edad" type="text" id="edad" /> <br />
+            Direccion: <input name="direccion" type="text" id="direccion" /> <br />
+ <input type="submit" name="Submit" value="Aceptar" />
+</form>
+-->
+<!-- Con GET -->
+            <form id="form1" name="form1" method="get" action="../../../CONTROL/1/E3/form.php">
+                Nombre: <input name="nombre" type="text" id="nombre" /> <br />
+                Apellido: <input name="apellido" type="text" id="apellido" /> <br />
+                Edad: <input name="edad" type="text" id="edad" /> <br />
+                Direccion: <input name="direccion" type="text" id="direccion" /> <br />
+                <input type="submit" name="Submit" value="Aceptar" />
+            </form>
+</body>
+</html>
