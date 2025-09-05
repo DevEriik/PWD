b50c0d0e51f7702b30afd7e1d800f7d514dcd1db
Erick Gonzalez <gempro30yt@gmail.com>
2025-09-05 09:49:02 -0300

    Fix

--- VISTA/1/E6/index.php
+++ VISTA/1/E6/index.php
@@ -9,6 +9,41 @@
 <body>
     <div class="conteiner">
         <form name="datos-alum" method="GET"  action="../../../CONTROL/1/E6/mensaje-alumno.php">
+            <label for="nombre">Nombre:</label>
+            <input type="text" name="nombre_form" id="nombre_form"> <br> <br>
+
+            <label for="apellido">Apellido:</label>
+            <input type="text" name="apellido_form" id="apellido_form"> <br> <br>
+
+            <label for="edad">Edad:</label>
+            <input type="number" name="edad_form" id="edad_form"> <br> <br>
+
+            <label for="direccion">Direccion:</label>
+            <input type="text" name="direccion_form" id="direccion_form"> <br> <br>
+
+            <label for="nivel-estudio">Nivel de Estudio</label> <br> <br>
+
+            <input type="radio" id="opcion1" name="opciones" value="valor1" required> 
+            <label for="no-estudio">No tiene estudio.</label>
+            <br><br>
+
+            <input type="radio" id="opcion2" name="opciones" value="valor2" required>
+            <label for="estudio-primario">Estudios Primarios.</label>
+            <br><br>
+
+            <input type="radio" id="opcion3" name="opciones" value="valor3" required>
+            <label for="estudio-secundario">Estudios Secundarios.</label>
+            <br><br>
+
+            <label for="sexo">Sexo</label>
+            <input list="sexos" id="tipo-sexo" name="tipo-sexo" required>
+            <datalist id="sexos">
+                <option value="masculino"></option>
+                <option value="femenino"></option>
+                <option value="otro"></option>
+            </datalist>
+            <br><br>
+            
             <label for="deportes">Indique los deportes que practica:</label> <br> <br>
 
             <input type="checkbox" name="deporte[]" value="futbol">
