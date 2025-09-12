cbfc4d03564b70fae951ebfdfb57da69c29a4d12
Erick Gonzalez <gempro30yt@gmail.com>
2025-09-11 21:15:45 -0300

    Navbar Create

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+//* Logica para decidir que vista cargar. 
+$page = $_GET['page'] ?? 'home'; 
+
+switch ($page) {
+    case 'tp1':
+        $view = 'views/1';
+        break;
+
+    case 'tp2':
+        $view = 'views/2';
+        break;
+    
+    case 'tp3':
+        $view = 'views/3';
+        break;
+    
+    default:
+        $view = '/index.php';
+        break;
+}
+?>
+
+<body>
+    <?php include 'views/structure/navbar.php' ?>
+
+    <div class="content">
+        <?php include $view ?>
+    </div>
+
+    <?php include 'views/structure/footer.php' ?>
+
+</body>
---
+++ views/structure/navbar.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="views/CSS/bootstrap.min.css">
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
+        <div class="container-fluid">
+            <a class="navbar-brand"  href="?page=home">Home</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex justify-content-center align-item-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="?page=tp1">TP 1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?page=tp2" class="nav-link">TP 2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="?page=tp3" class="nav-link">TP 3</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <Script src="views/JS/bootstrap.bundle.min.js"></Script>
+</body>
